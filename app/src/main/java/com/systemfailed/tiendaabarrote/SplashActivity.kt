6ead1802353f34c4cff79d678a3db9359b068139
JCJMA1994 <201214026@uns.edu.pe>
2022-07-18 23:21:48 -0500

    redireccion a LoginActivity.kt

--- app/src/main/java/com/systemfailed/tiendaabarrote/SplashActivity.kt
+++ app/src/main/java/com/systemfailed/tiendaabarrote/SplashActivity.kt
@@ -3,10 +3,11 @@ package com.systemfailed.tiendaabarrote
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.systemfailed.tiendaabarrote.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
