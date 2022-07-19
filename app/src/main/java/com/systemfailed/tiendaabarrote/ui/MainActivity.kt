6c5212e27e0024ebd63647a9a43583cb6780e11c
JCJMA1994 <201214026@uns.edu.pe>
2022-07-19 12:22:24 -0500

    cmabios strings.xml

--- app/src/main/java/com/systemfailed/tiendaabarrote/ui/MainActivity.kt
+++ app/src/main/java/com/systemfailed/tiendaabarrote/ui/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.systemfailed.tiendaabarrote
+package com.systemfailed.tiendaabarrote.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.systemfailed.tiendaabarrote.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
