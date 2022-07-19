d8385be2ada1fc29dca8cbce3f98766e2789428a
JCJMA1994 <201214026@uns.edu.pe>
2022-07-18 22:01:04 -0500

    una vez mostrado el splash direccionar a la pantalla principal

--- app/src/main/java/com/systemfailed/tiendaabarrote/SplashActivity.kt
+++ app/src/main/java/com/systemfailed/tiendaabarrote/SplashActivity.kt
@@ -1,10 +1,12 @@
 package com.systemfailed.tiendaabarrote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
